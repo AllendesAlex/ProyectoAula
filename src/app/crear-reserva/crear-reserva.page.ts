1e685c127b88d3415cd5f0c8088dc72291c60b6b
HalconNocturno <167113946+HalconNocturno@users.noreply.github.com>
2024-10-14 13:35:32 -0300

    Estilos Crear-reserva

--- src/app/crear-reserva/crear-reserva.page.ts
+++ src/app/crear-reserva/crear-reserva.page.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { ReservaService } from '../servicios/reserva.service';
+import { Router } from '@angular/router'; 
+import { AuthService } from '../servicios/auth.service';
 @Component({
   selector: 'app-crear-reserva',
   templateUrl: './crear-reserva.page.html',
   styleUrls: ['./crear-reserva.page.scss'],
 })
-export class CrearReservaPage {
+export class CrearReservaPage implements OnInit {
   nuevaReserva: any = {};
+  username: string = '';
 
-  constructor(private reservaService: ReservaService) {}
+  constructor(private reservaService: ReservaService, private router: Router, private authService: AuthService) {
+    this.username = this.authService.getUsername(); 
+  }
+  ngOnInit() {
+    this.username = this.authService.getUsername();
+  }
 
   crearReserva() {
     this.reservaService.crearReserva(this.nuevaReserva).subscribe(response => {
     });
+  }
+
+  goBack() {
+    this.router.navigate(['/menu']);
   }
 }
